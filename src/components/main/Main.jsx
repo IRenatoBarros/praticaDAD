f4f6023ce9e0204a46cea3d342e7a61caf90c3a4
IRenatoBarros <renatofilho9722@gmail.com>
2024-09-17 12:29:22 -0300

    fiz

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -8,13 +8,13 @@ import errado from '../../assets/errado.png'
 function Main(){
     return(
         <div className={style.geral}>
-            <h1>Plans +</h1>
+            <h1 className={style.title}>Plans +</h1>
             <div className={style.group}>
                 <CardSell title='FREE' price='$0' yesSrc={correto} yesSrc2={errado} yesSrc3={errado} yesSrc4={errado} colorBotton='red'></CardSell>
                 <CardSell title='PREMIUM' price='$99' yesSrc={correto} yesSrc2={correto} yesSrc3={errado} yesSrc4={errado} colorBotton='red'></CardSell>
                 <CardSell title='PRO' price='$199' yesSrc={correto} yesSrc2={correto} yesSrc3={correto} yesSrc4={correto} colorBotton='red'></CardSell>
             </div>
-            <h1>New Member</h1>
+            <h1 className={style.title}>New Member</h1>
             <div className={style.group}>
                 <CardSell title='FREE' price='$0' yesSrc={correto} yesSrc2={errado} yesSrc3={errado} yesSrc4={errado} colorBotton='green'></CardSell>
                 <CardSell title='PREMIUM' price='$199' yesSrc={correto} yesSrc2={correto} yesSrc3={errado} yesSrc4={errado} colorBotton='green'></CardSell>
